a30d69e7432a92339cafee9d4659dec611e8a4e2
rhynerj <43280664+rhynerj@users.noreply.github.com>
2024-05-28 19:13:09 -0700

    Update sr_functions.py
    Add matplotlib backend import for PyInstaller

--- src/sr_functions.py
+++ src/sr_functions.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.backends.backend_ps  # included to prompt PyInstaller import of correct backend
 import seaborn as sns
 
 import math
