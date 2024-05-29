2aa27d73f94f5d92ee9ecf8b8ad1c572625d64fe
rhynerj <43280664+rhynerj@users.noreply.github.com>
2024-05-28 19:17:37 -0700

    Update build_SR_executable.py
    1. Updates to reflect added src files
    2. Updates to ensure all necessary packages are imported

--- build_SR_executable.py
+++ build_SR_executable.py
@@ -6,12 +6,14 @@ sys.setrecursionlimit(50000)
 PyInstaller.__main__.run([
     '--name=%s' %'ScaredyRat',
     '--onefile',
-    '--hidden-import=%s' %'tkinter,scipy,numpy,pandas,matplotlib,seaborn,PySimpleGUI',
+    '--hidden-import=%s' %'tkinter,scipy,numpy,pandas,openpyxl,matplotlib,seaborn,PySimpleGUI',
+    '--additional-hooks-dir=%s' %'.',
     '--clean',
- #   '--exclude-module=torch',
     '--upx-dir=%s\,%''./UPX/upx-3.96-win64',
-    # '--windowed',
     '--paths=%s' %'./src',
     './SR_GUI.py',
-    './src/sr_functions.py'
+    './src/sr_compiled.py',
+    './src/sr_functions.py',
+    './src/sr_individual.py',
+    './src/sr_settings.py'
 ])
